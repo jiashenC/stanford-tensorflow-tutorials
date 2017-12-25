0ef386293e74235eea602e7ab43dda270b77a73b
Jiashen Cao <caojiashen24@gmail.com>
2017-12-25 20:37:53 +0800

    convolutional NN for MNIST

--- examples/07_convnet_mnist_starter.py
+++ examples/07_convnet_mnist_starter.py
@@ -9,9 +9,10 @@ from __future__ import division
 from __future__ import print_function
 
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
-import time 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import time
 
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
@@ -55,42 +56,36 @@ with tf.variable_scope('conv1') as scope:
     # first, reshape the image to [BATCH_SIZE, 28, 28, 1] to make it work with tf.nn.conv2d
     # use the dynamic dimension -1
     images = tf.reshape(X, shape=[-1, 28, 28, 1])
-    
-    # TO DO
 
     # create kernel variable of dimension [5, 5, 1, 32]
     # use tf.truncated_normal_initializer()
-    
-    # TO DO
+    kernel = tf.get_variable('kernels', [5, 5, 1, 32], initializer=tf.truncated_normal_initializer())
 
     # create biases variable of dimension [32]
     # use tf.constant_initializer(0.0)
-    
-    # TO DO 
+    biases = tf.get_variable('biases', [32], initializer=tf.constant_initializer(0.0))
 
     # apply tf.nn.conv2d. strides [1, 1, 1, 1], padding is 'SAME'
-    
-    # TO DO
+    conv = tf.nn.conv2d(images, kernel, strides=[1, 1, 1, 1], padding='SAME')
 
     # apply relu on the sum of convolution output and biases
-    
-    # TO DO 
+    conv1 = tf.nn.relu(conv + biases, name=scope.name)
 
     # output is of dimension BATCH_SIZE x 28 x 28 x 32
 
 with tf.variable_scope('pool1') as scope:
     # apply max pool with ksize [1, 2, 2, 1], and strides [1, 2, 2, 1], padding 'SAME'
-    
-    # TO DO
+    pool1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
+                           padding='SAME')
 
-    # output is of dimension BATCH_SIZE x 14 x 14 x 32
+# output is of dimension BATCH_SIZE x 14 x 14 x 32
 
 with tf.variable_scope('conv2') as scope:
     # similar to conv1, except kernel now is of the size 5 x 5 x 32 x 64
-    kernel = tf.get_variable('kernels', [5, 5, 32, 64], 
-                        initializer=tf.truncated_normal_initializer())
+    kernel = tf.get_variable('kernels', [5, 5, 32, 64],
+                             initializer=tf.truncated_normal_initializer())
     biases = tf.get_variable('biases', [64],
-                        initializer=tf.random_normal_initializer())
+                             initializer=tf.random_normal_initializer())
     conv = tf.nn.conv2d(pool1, kernel, strides=[1, 1, 1, 1], padding='SAME')
     conv2 = tf.nn.relu(conv + biases, name=scope.name)
 
@@ -99,25 +94,23 @@ with tf.variable_scope('conv2') as scope:
 with tf.variable_scope('pool2') as scope:
     # similar to pool1
     pool2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
-                            padding='SAME')
+                           padding='SAME')
 
     # output is of dimension BATCH_SIZE x 7 x 7 x 64
 
 with tf.variable_scope('fc') as scope:
     # use weight of dimension 7 * 7 * 64 x 1024
     input_features = 7 * 7 * 64
-    
-    # create weights and biases
 
-    # TO DO
+    # create weights and biases
+    w = tf.get_variable('weights', [input_features, 1024], initializer=tf.random_normal_initializer())
+    b = tf.get_variable('biases', [1024], initializer=tf.random_normal_initializer())
 
     # reshape pool2 to 2 dimensional
     pool2 = tf.reshape(pool2, [-1, input_features])
 
     # apply relu on matmul of pool2 and w + b
     fc = tf.nn.relu(tf.matmul(pool2, w) + b, name='relu')
-    
-    # TO DO
 
     # apply dropout
     fc = tf.nn.dropout(fc, dropout, name='relu_dropout')
@@ -125,63 +118,52 @@ with tf.variable_scope('fc') as scope:
 with tf.variable_scope('softmax_linear') as scope:
     # this you should know. get logits without softmax
     # you need to create weights and biases
-
-    # TO DO
+    w = tf.get_variable('weights', [1024, N_CLASSES], initializer=tf.random_normal_initializer())
+    b = tf.get_variable('biases', [N_CLASSES], initializer=tf.random_normal_initializer())
+    logits = tf.matmul(fc, w) + b
 
 # Step 6: define loss function
 # use softmax cross entropy with logits as the loss function
 # compute mean cross entropy, softmax is applied internally
 with tf.name_scope('loss'):
     # you should know how to do this too
-    
-    # TO DO
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y), name='loss')
 
 # Step 7: define training op
 # using gradient descent with learning rate of LEARNING_RATE to minimize cost
 # don't forgot to pass in global_step
-
-# TO DO
+optimizer = tf.train.AdamOptimizer(LEARNING_RATE).minimize(loss, global_step=global_step)
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    saver = tf.train.Saver()
-    # to visualize using TensorBoard
-    writer = tf.summary.FileWriter('./my_graph/mnist', sess.graph)
-    ##### You have to create folders to store checkpoints
-    ckpt = tf.train.get_checkpoint_state(os.path.dirname('checkpoints/convnet_mnist/checkpoint'))
-    # if that checkpoint exists, restore from checkpoint
-    if ckpt and ckpt.model_checkpoint_path:
-        saver.restore(sess, ckpt.model_checkpoint_path)
-    
     initial_step = global_step.eval()
 
     start_time = time.time()
     n_batches = int(mnist.train.num_examples / BATCH_SIZE)
 
     total_loss = 0.0
-    for index in range(initial_step, n_batches * N_EPOCHS): # train the model n_epochs times
+    for index in range(initial_step, n_batches * N_EPOCHS):  # train the model n_epochs times
         X_batch, Y_batch = mnist.train.next_batch(BATCH_SIZE)
-        _, loss_batch = sess.run([optimizer, loss], 
-                                feed_dict={X: X_batch, Y:Y_batch, dropout: DROPOUT}) 
+        _, loss_batch = sess.run([optimizer, loss],
+                                 feed_dict={X: X_batch, Y: Y_batch, dropout: DROPOUT})
         total_loss += loss_batch
         if (index + 1) % SKIP_STEP == 0:
             print('Average loss at step {}: {:5.1f}'.format(index + 1, total_loss / SKIP_STEP))
             total_loss = 0.0
-            saver.save(sess, 'checkpoints/convnet_mnist/mnist-convnet', index)
-    
-    print("Optimization Finished!") # should be around 0.35 after 25 epochs
+
+    print("Optimization Finished!")  # should be around 0.35 after 25 epochs
     print("Total time: {0} seconds".format(time.time() - start_time))
-    
+
     # test the model
-    n_batches = int(mnist.test.num_examples/BATCH_SIZE)
+    n_batches = int(mnist.test.num_examples / BATCH_SIZE)
     total_correct_preds = 0
     for i in range(n_batches):
         X_batch, Y_batch = mnist.test.next_batch(BATCH_SIZE)
-        _, loss_batch, logits_batch = sess.run([optimizer, loss, logits], 
-                                        feed_dict={X: X_batch, Y:Y_batch, dropout: DROPOUT}) 
+        _, loss_batch, logits_batch = sess.run([optimizer, loss, logits],
+                                               feed_dict={X: X_batch, Y: Y_batch, dropout: DROPOUT})
         preds = tf.nn.softmax(logits_batch)
         correct_preds = tf.equal(tf.argmax(preds, 1), tf.argmax(Y_batch, 1))
         accuracy = tf.reduce_sum(tf.cast(correct_preds, tf.float32))
-        total_correct_preds += sess.run(accuracy)   
-    
-    print("Accuracy {0}".format(total_correct_preds/mnist.test.num_examples))
+        total_correct_preds += sess.run(accuracy)
+
+    print("Accuracy {0}".format(total_correct_preds / mnist.test.num_examples))
